4fdd216f64d47a75f6eb4da9f42d4e5ab17ae076
dydwo0809 <84170315+dydwo0809@users.noreply.github.com>
2022-06-08 22:28:01 +0900

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -19,17 +19,21 @@ public:
     for (const auto& location : locations)
       mvaddch(location.first, location.second, '#');
   }
-  void move(pair<int, int> nextHead){
-    //방향 입력받음
-    //pair<int, int> nextHead = {a, b}; // 입력받은 방향으로 가야함
-    locations.push_front(nextHead); //새로운 머리 추가
-    mvaddch(nextHead.first, nextHead.second, '#');
+  void remove_tale(){
     pair<int, int> remove_tale = locations.back(); // 맨 끝 꼬리 제거
     locations.pop_back();
     mvaddch(remove_tale.first, remove_tale.second, ' ');
   }
+  void add_head(pair<int, int> nextHead){
+    locations.push_front(nextHead); //새로운 머리 추가
+    mvaddch(nextHead.first, nextHead.second, '#');
+  }
+  void move(pair<int, int> nextHead){
+    locations.push_front(nextHead); //새로운 머리 추가
+    mvaddch(nextHead.first, nextHead.second, '#');
+    remove_tale(); // 맨 끝 꼬리 제거
+  }
   pair<int, int> getHead(){
     return locations.front();
   }
-
 };
