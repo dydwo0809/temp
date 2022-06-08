ecead2944cabc91d11cc54cf5eec09cb8def44f8
dydwo0809 <84170315+dydwo0809@users.noreply.github.com>
2022-06-08 22:28:47 +0900

    Update SnakeGame.cpp

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -2,9 +2,14 @@
 ///// SnakeGame.cpp /////
 /////////////////////////
 
+#include <unistd.h>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 #include "SnakeGame.hpp"
-
 #include "Snake.cpp"
+#include "Item.cpp"
+
 
 #define DEBUG_CHECK(__CONDITION__, __MESSAGE__)\
 {\
@@ -21,6 +26,25 @@ SnakeGame::SnakeGame()
 {
     this->InitMainScreen();
     this->MainOperation();
+}
+
+static inline bool
+kbhit(void){
+    int ch;
+    bool ret;
+
+    nodelay(stdscr, TRUE);
+
+    ch = getch();
+    if ( ch == ERR ) {
+        ret = false;
+    } else {
+        ret = true;
+        ungetch(ch); // 마지막에 받은 문자를 버퍼에 다시 넣어서 다음 getch()가 받을 수 있도록 합니다.
+    }
+
+    nodelay(stdscr, FALSE);
+    return ret;
 }
 
 void SnakeGame::InitMainScreen()
@@ -50,35 +74,6 @@ void SnakeGame::InitMainScreen()
     mvwprintw(gameWindow, 1, 1, "This is Game Window");
     wattroff(gameWindow, COLOR_PAIR(gameWindowColorPair));
     wrefresh(gameWindow);
-
-    Snake snake = Snake();
-    keypad((stdscr), TRUE);
-    int ch;
-    while(ch = getch()){
-      clear;
-      if (ch == KEY_F(1)) break;
-
-      pair<int, int> temp = snake.getHead();
-      pair<int, int> nextHead;
-
-      if (ch == KEY_LEFT){
-        nextHead = {temp.first, temp.second - 1};
-      }
-      else if (ch == KEY_RIGHT){
-        nextHead = {temp.first, temp.second + 1};
-      }
-      else if (ch == KEY_UP){
-        nextHead = {temp.first - 1, temp.second};
-      }
-      else if (ch == KEY_DOWN){
-        nextHead = {temp.first + 1, temp.second};
-      }
-      snake.move(nextHead);
-    }
-    mvaddch(3,3,'%');
-    mvaddch(3,67,'%');
-    mvaddch(32,3,'%');
-    mvaddch(32,67,'%');
 
 
     scoreWindow = newwin(4, 15, 3, 75);
@@ -112,6 +107,124 @@ void SnakeGame::InitMainScreen()
     mvwprintw(debugWindow, 0, 0, "Debug Console:");
     wattroff(debugWindow, COLOR_PAIR(debugWindowColorPair));
     wrefresh(debugWindow);
+
+    mvaddch(3,3,'a');
+    mvaddch(3,67,'b');
+    mvaddch(32,3,'c');
+    mvaddch(32,67,'d');
+
+    Snake snake = Snake();
+    keypad((stdscr), TRUE);
+    int ch;
+    int dx = 0;
+    int dy = 1;
+
+    vector<Item> Items;
+    pair<int, int> temp;
+    srand(time(NULL));
+    do{
+      temp = {rand() % 29 + 4, rand() % 63 + 4};
+    }while(FALSE); // 스네이크랑 겹치지 않는지 확인
+    Item item1 = Item('G', temp);
+
+    do{
+      temp = {rand() % 29 + 4, rand() % 63 + 4};
+    }while(FALSE); // 스네이크랑 겹치지 않는지 확인
+    Item item2 = Item('G', temp);
+
+    do{
+      temp = {rand() % 29 + 4, rand() % 63 + 4};
+    }while(FALSE); // 스네이크랑 겹치지 않는지 확인
+    Item item3 = Item('P', temp);
+
+    Items.push_back(item1);
+    Items.push_back(item2);
+    Items.push_back(item3);
+
+    for (int i = 0; i < 3; i++){
+      Items[i].print();
+    }
+
+    while(TRUE){
+      usleep(500000);
+      for (int i = 0; i < 3; i++){
+        Items[i].time++;
+      }
+
+
+      if (kbhit()){
+        ch = getch();
+        if (ch == KEY_F(1)) break;
+
+        if (ch == KEY_LEFT){
+          if (dx == 0 && dy == -1) break;
+          dx = 0;
+          dy = -1;
+        }
+        else if (ch == KEY_RIGHT){
+          if (dx == 0 && dy == 1) break;
+          dx = 0;
+          dy = 1;
+        }
+        else if (ch == KEY_UP){
+          if (dx == -1 && dy == 0) break;
+          dx = -1;
+          dy = 0;
+        }
+        else if (ch == KEY_DOWN){
+          if (dx == 1 && dy == 0) break;
+          dx = 1;
+          dy = 0;
+        }
+      }
+
+      pair<int, int> temp = snake.getHead();
+      pair<int, int> nextHead;
+
+      nextHead = {temp.first + dx, temp.second + dy};
+
+      // nextHead가 갈 수 있는 곳인지, 아이템 있는지 확인 확인하는 코드 넣을곳
+      for (int i = 0; i < 3; i++){
+        if (nextHead == Items[i].Point){
+            if (Items[i].ItemType == 'P'){
+              snake.remove_tale();
+            }
+            else{
+              snake.add_head(nextHead);
+            }
+            if (kbhit()){
+              ch = getch();
+              if (ch == KEY_F(1)) break;
+
+              if (ch == KEY_LEFT){
+                if (dx == 0 && dy == -1) break;
+                dx = 0;
+                dy = -1;
+              }
+              else if (ch == KEY_RIGHT){
+                if (dx == 0 && dy == 1) break;
+                dx = 0;
+                dy = 1;
+              }
+              else if (ch == KEY_UP){
+                if (dx == -1 && dy == 0) break;
+                dx = -1;
+                dy = 0;
+              }
+              else if (ch == KEY_DOWN){
+                if (dx == 1 && dy == 0) break;
+                dx = 1;
+                dy = 0;
+              }
+            }
+
+            pair<int, int> temp = snake.getHead();
+            pair<int, int> nextHead;
+        }
+      }
+
+      snake.move(nextHead);
+    }
 }
 
 void SnakeGame::MainOperation()
